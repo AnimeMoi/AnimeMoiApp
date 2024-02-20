219bde459f4e2245f8611363219a683fc08ecace
Hoang3402 <nvh.02021995@gmail.com>
2024-02-20 14:51:37 +0700

    Done component "Bar"

--- app/src/main/java/com/example/animemoi_app/MainActivity.kt
+++ app/src/main/java/com/example/animemoi_app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.animemoi_app.common.Bar
 import com.example.animemoi_app.ui.theme.AnimeMoi_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Bar()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeMoi_AppTheme {
-        Greeting("Android")
     }
 }
