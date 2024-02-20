3e2f2918c7c25b3ff76edda454f44bdc32f308fd
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-02-20 16:11:27 +0700

    Design Button Common Component

--- app/src/main/java/com/example/animemoi_app/MainActivity.kt
+++ app/src/main/java/com/example/animemoi_app/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.animemoi_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.animemoi_app.common.ButtonCommon
 import com.example.animemoi_app.ui.theme.AnimeMoi_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +18,9 @@ class MainActivity : ComponentActivity() {
             AnimeMoi_AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +28,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting() {
+    ButtonCommon(
+        text = "Click me",
+        onClick = { /* TODO: Handle button click */ },
+        iconButton = Icons.Default.AccountBox,
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeMoi_AppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/animemoi_app/common/Button.kt
+++ app/src/main/java/com/example/animemoi_app/common/Button.kt
@@ -1,2 +1,61 @@
 package com.example.animemoi_app.common
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ButtonCommon(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    contentColor: Color = Color.White,
+    backgroundColor: Color = Color(0xFFFF6666),
+    iconButton: ImageVector? = null,
+    ) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .background(color = backgroundColor, shape = CircleShape),
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = contentColor
+        )
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        if (iconButton != null) {
+            Icon(
+                imageVector = iconButton,
+                contentDescription = "",
+                Modifier.size(20.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ButtonCommonPreview() {
+    ButtonCommon(
+        text = "Click me",
+        onClick = { /* TODO: Handle button click */ },
+        iconButton = Icons.Default.AccountBox
+    )
+}
