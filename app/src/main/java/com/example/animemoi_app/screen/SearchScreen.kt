c2c9d1172e7d91f8f7f9848d37847c56d7066bc8
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-02-22 22:24:49 +0700

    Design Search Screen Category

--- app/src/main/java/com/example/animemoi_app/screen/SearchScreen.kt
+++ app/src/main/java/com/example/animemoi_app/screen/SearchScreen.kt
@@ -1,11 +1,13 @@
 package com.example.animemoi_app.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,11 +18,19 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -28,6 +38,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +47,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.animemoi_app.common.ComeBack
 import com.example.animemoi_app.common.ListSourceComic
 import com.example.animemoi_app.common.searchBar
+import java.util.Locale.Category
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -98,30 +110,49 @@ fun CategorySearch(categories: List<String>) {
             .padding(16.dp, 8.dp)
     ) {
         Divider(color = Color(0xFF808080))
-        val screenSize = LocalConfiguration.current.screenWidthDp.dp
         LazyVerticalGrid(
-            GridCells.Fixed(3),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            GridCells.Adaptive(60.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(0.dp, 16.dp)
         ) {
             items(categories) { category ->
                 // Your category item composable
-                Box(
-                    modifier = Modifier
-                        .background(Color.Gray)
-                        .padding(16.dp)
-                        .clickable {  }
-                ) {
-                    Text(
-                        text = category,
-                        modifier = Modifier
-                            .padding(8.dp),
-                        color = Color.White
-                    )
-                }
+                Category(category)
             }
         }
     }
+}
+
+@Composable
+fun Category(category: String) {
+    var clickCount by remember { mutableIntStateOf(0) }
+    Surface(
+        shape = CircleShape,
+        content = {
+            Text(
+                text = category,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                color = when (clickCount) {
+                    1, 2 -> Color.White
+                    else -> Color.Black},
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .background(
+                        when (clickCount) {
+                            1 -> Color(0xFF33CC66)
+                            2 -> Color(0xFFFF6666)
+                            else -> Color(0xFF999999)
+                        }
+                    )
+                    .clickable {
+                        clickCount++
+                        if (clickCount == 3) clickCount = 0
+                    }
+            )
+        }
+    )
 }
 
 @Composable
@@ -149,7 +180,7 @@ fun PreviousSearch(search: String) {
     ) {
         Text(
             text = search,
-            fontSize = 8.sp,
+            fontSize = 12.sp,
             color = Color(0xFF808080),
             fontWeight = FontWeight.Normal,
         )
