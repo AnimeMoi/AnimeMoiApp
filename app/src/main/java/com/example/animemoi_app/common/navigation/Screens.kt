5f1d4ee77d48400806b55e86a472ba5a884239b5
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-02-23 20:50:23 +0700

    Done basic NotificationScreen

--- app/src/main/java/com/example/animemoi_app/common/navigation/Screens.kt
+++ app/src/main/java/com/example/animemoi_app/common/navigation/Screens.kt
@@ -5,5 +5,6 @@ enum class Screens {
     HistoryScreen,
     SearchScreen,
     SettingScreen,
-    CategoryScreen
+    CategoryScreen,
+    NotificationScreen
 }
--- app/src/main/java/com/example/animemoi_app/screen/HomeScreen.kt
+++ app/src/main/java/com/example/animemoi_app/screen/HomeScreen.kt
@@ -2,33 +2,29 @@ package com.example.animemoi_app.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import com.example.animemoi_app.common.Bar
 import com.example.animemoi_app.common.ListSourceComic
 import com.example.animemoi_app.common.comic.ComicRow
 import com.example.animemoi_app.common.comic.ComicRowWithTitle
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavHostController) {
     val sources = listOf("NetTruyen", "BaoTangTruyen", "Yurineko")
     Column {
-        Bar()
+        Bar(navController)
         ListSourceComic(sources)
-        LazyColumn {
-            item {
-                ComicRow()
-                ComicRowWithTitle(title = "Truyện mới đăng")
-                ComicRowWithTitle(title = "Truyện hay")
-                ComicRowWithTitle(title = "Truyện mới cập nhật")
-                ComicRowWithTitle(title = "Truyện đầy đủ")
-            }
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            ComicRow()
+            ComicRowWithTitle(title = "Truyện mới đăng")
+            ComicRowWithTitle(title = "Truyện hay")
+            ComicRowWithTitle(title = "Truyện mới cập nhật")
+            ComicRowWithTitle(title = "Truyện đầy đủ")
         }
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen()
 }
