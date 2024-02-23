ae4cfb85c9ed35ea2bdec3002331891fe85bd204
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-02-23 08:51:22 +0700

    Done search page result (#17)

---
+++ app/src/main/java/com/example/animemoi_app/screen/SearchResultScreen.kt
@@ -0,0 +1,18 @@
+package com.example.animemoi_app.screen
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.animemoi_app.common.ComeBack
+import com.example.animemoi_app.common.comic.ComicGrid
+
+@Composable
+fun SearchResultScreen(navController: NavHostController) {
+    ComeBack(title = "Kết quả", navController)
+    Spacer(modifier = Modifier.height(16.dp))
+    ComicGrid()
+}
+
