421051a0e52f3e4efbd625ba2c900675a7c2c152
vule2303 <leductuanvu5@gmail.com>
2024-02-25 12:04:03 +0700

    Im finished it.

--- app/src/main/java/com/example/animemoi_app/screen/HistoryScreen.kt
+++ app/src/main/java/com/example/animemoi_app/screen/HistoryScreen.kt
@@ -2,7 +2,6 @@ package com.example.animemoi_app.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,18 +11,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.animemoi_app.R
 import com.example.animemoi_app.common.Bar
 import com.example.animemoi_app.common.history.GridHistoryCard
-import com.example.animemoi_app.common.history.HistoryCard
-import com.example.animemoi_app.model.Comic
 
 @Composable
 fun HistoryScreen(navController: NavHostController){
