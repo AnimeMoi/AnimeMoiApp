86c1289a8e544fbdc7a6570e78efdc7a56786047
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-02-27 00:09:39 +0700

    Component and Setting Page
    - Tittle with Icon Component
    - Input hide and visible Component
    - Design Setting Source Comic

--- app/src/main/java/com/example/animemoi_app/screen/SettingScreen.kt
+++ app/src/main/java/com/example/animemoi_app/screen/SettingScreen.kt
@@ -5,23 +5,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -40,16 +49,20 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.animemoi_app.R
 import com.example.animemoi_app.common.ButtonCommon
 import com.example.animemoi_app.common.ComeBack
+import com.example.animemoi_app.common.setting.Input
+import com.example.animemoi_app.common.setting.TitleWithIcon
 
 @Composable
 fun SettingScreen(navController: NavHostController) {
@@ -67,6 +80,91 @@ fun SettingScreen(navController: NavHostController) {
             )
         )
         LoginRegisterFrame()
+        SettingSourceFrame()
+    }
+}
+
+@Composable
+fun ListSource() {
+    val listSource = listOf(
+        SourceData(painterResource(id = R.drawable.deba), "Nettruyen"),
+        SourceData(painterResource(id = R.drawable.thegioihoanmy), "Tachiyomi"),
+        SourceData(painterResource(id = R.drawable.requytroicho480197), "Tu tiên truyện")
+    )
+    LazyVerticalGrid(
+        GridCells.Fixed(1),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(listSource) { source ->
+            Source(source = source)
+        }
+    }
+}
+
+@Composable
+fun Source(source: SourceData) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = source.image,
+            contentDescription = "Image",
+            Modifier.size(40.dp)
+        )
+        Text(
+            text = source.name,
+            color = Color.White,
+            modifier = Modifier
+                .padding(16.dp, 0.dp)
+                .fillMaxWidth(0.9f)
+        )
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Icon",
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun SettingSourceFrame() {
+    Column(
+        modifier = Modifier
+            .padding(16.dp, 0.dp, 16.dp, 16.dp)
+            .fillMaxWidth()
+            .background(Color(0xFF4D4D4D), RoundedCornerShape(10.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Cài đặt nguồn truyện",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(0.dp, 0.dp, 8.dp, 0.dp)
+            )
+            Input(text = "NSFW")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0f, 0f, 0f, 0.5f), RoundedCornerShape(10.dp))
+            ) {
+                TitleWithIcon(
+                    title = "Nguồn truyện của bạn",
+                    icon = Icons.Default.KeyboardArrowUp
+                )
+                Box {
+                    Divider(color = Color(0xFF808080), modifier = Modifier.padding(8.dp))
+                }
+                TitleWithIcon(title = "Thêm nguồn mới", icon = Icons.Default.Add)
+                ListSource()
+            }
+        }
     }
 }
 
@@ -439,7 +537,8 @@ fun PersonalInformation(user: UserData) {
 @Preview(showBackground = true)
 @Composable
 fun SettingScreenPreview() {
-    SettingScreen(rememberNavController())
+    SettingScreen(navController = rememberNavController())
 }
 
 data class UserData(val image: Painter, var name: String, var password: String)
+data class SourceData(val image: Painter, var name: String)
