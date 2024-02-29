cd2af10991dfa6f58f52bb629da4bcf6edc2f4bf
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-03-01 00:19:14 +0700

    last fix

--- app/src/main/java/com/example/animemoi_app/model/SourceComic.kt
+++ app/src/main/java/com/example/animemoi_app/model/SourceComic.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import com.example.animemoi_app.R
 
 data class SourceComic(
-    @DrawableRes val icon: Int = R.drawable.book_4,
+    @DrawableRes val icon: Int = R.drawable.hentaivn,
     val name: String,
     val label: String,
     val nsfw: Boolean
